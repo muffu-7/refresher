8854d025d62929fe44f7b7721d82c5d74763ec63
muffu-7 <tati.mufi@gmail.com>
2024-09-01 14:03:24 +0530

    Update refresh.py

--- refresh.py
+++ refresh.py
@@ -5,12 +5,15 @@ import requests
 load_dotenv()
 
 def wake_up_call():
-    url = os.getenv('URL')
-    response = requests.get(f'{url}')
-    if response.status_code == 200:
-        print('Wake up call successful')
-    else:
-        print('Wake up call failed')
+    try:
+        url = os.getenv('URL')
+        response = requests.get(f'{url}')
+        if response.status_code == 200:
+            print('Wake up call successful')
+        else:
+            print('Wake up call failed')
+    except Exception as e:
+        print('Error occurred while making the API call', e)
 
 if __name__ == "__main__":
     wake_up_call()
