75176c86015670c6fcd51db7b529b400b824cc6f
Mufaddal Tatiwala <tati.mufi@gmail.com>
2024-09-01 18:19:13 +0530

    update refresh script to call two APIs

--- refresh.py
+++ refresh.py
@@ -6,12 +6,18 @@ load_dotenv()
 
 def wake_up_call():
     try:
-        url = os.getenv('URL')
-        response = requests.get(f'{url}')
+        url_1 = os.getenv('URL1')
+        url_2 = os.getenv('URL2')
+        response = requests.get(f'{url_1}')
         if response.status_code == 200:
-            print('Wake up call successful')
+            print('Wake up call successful - 1')
         else:
-            print('Wake up call failed')
+            print('Wake up call failed - 1')
+        response = requests.get(f'{url_2}')
+        if response.status_code == 200:
+            print('Wake up call successful - 2')
+        else:
+            print('Wake up call failed - 2')
     except Exception as e:
         print('Error occurred while making the API call', e)
 
