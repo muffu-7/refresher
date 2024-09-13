62da6c593bc64d63def8ebac11078dd9321b8788
Mufaddal Tatiwala <tati.mufi@gmail.com>
2024-09-13 19:50:45 +0530

    Add new wakeup call

--- refresh.py
+++ refresh.py
@@ -9,6 +9,7 @@ def wake_up_call():
         url_1 = os.getenv('URL1')
         url_2 = os.getenv('URL2')
         url_3 = os.getenv('URL3')
+        url_4 = os.getenv('URL4')
         response = requests.get(f'{url_1}')
         if response.status_code == 200:
             print('Wake up call successful - 1')
@@ -24,6 +25,11 @@ def wake_up_call():
             print('Wake up call successful - 3')
         else:
             print('Wake up call failed - 3')
+        response = requests.get(f'{url_4}')
+        if response.status_code == 200:
+            print('Wake up call successful - 4')
+        else:
+            print('Wake up call failed - 4')
     except Exception as e:
         print('Error occurred while making the API call', e)
 
